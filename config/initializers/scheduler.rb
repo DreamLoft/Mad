79d43567ecda75bed8af8bfa4727ab798a78448c
Rakesh Sharma <sharma26.riti@gmail.com>
2017-02-23 19:01:20 +0530

    Update scheduler.rb

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -7,7 +7,7 @@ s = Rufus::Scheduler.singleton
 
 # Stupid recurrent task...
 #
-s.every '24h' , :first_at => "#{Date.today} 19:05:00 +0530" do
+s.every '24h' , :first_at => "#{Date.today} 19:35:00 +0530" do
 
   @user = User.find_by(email: "sharbani.samanta@quantumcs.com")
   UserMailer.report_email(@user).deliver_now
