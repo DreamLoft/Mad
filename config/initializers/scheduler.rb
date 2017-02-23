3f72b1d8815a5242c2a3f950c21fb58b65488f0d
Rakesh Sharma <sharma26.riti@gmail.com>
2017-02-23 19:42:28 +0530

    Update scheduler.rb

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,14 +1,14 @@
-#require 'rufus-scheduler'
+require 'rufus-scheduler'
 
 # Let's use the rufus-scheduler singleton
 #
-#s = Rufus::Scheduler.singleton
+s = Rufus::Scheduler.singleton
 
 
 # Stupid recurrent task...
 #
-#s.every '24h' , :first_at => "#{Date.today} 20:05:00 +0530" do
+s.every '24h' , :first_at => "#{Date.today} 20:05:00 +0530" do
 
- # @user = User.find(2)
-#  UserMailer.report_email(@user).deliver_now
-#end
+ @user = User.find(2)
+  UserMailer.report_email(@user).deliver_now
+end
